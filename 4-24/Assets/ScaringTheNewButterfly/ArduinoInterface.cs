f70fc006241d72791e137e0a20de1c8b9d040a57
Jacob Gold <goldjacob29@cs.uchicago.edu>
2019-04-30 03:04:40 -0500

    4-24

---
+++ 4-24/Assets/ScaringTheNewButterfly/ArduinoInterface.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoInterface : MonoBehaviour {
+
+    SerialPort interfaceStream;
+    public string interfacePath = "COM3";
+    public int baudRate = 9600;
+
+    Animator butterflyAnimator;
+    
+	void Start () {
+        butterflyAnimator = gameObject.GetComponent<Animator>();
+        try
+        {
+            interfaceStream = new SerialPort(interfacePath, baudRate);
+            interfaceStream.Open();
+            interfaceStream.ReadTimeout = 1;
+            Debug.Log("Opened.");
+        } catch (System.IO.IOException e)
+        {
+            Debug.Log("Could not find the serial port named: " + interfacePath + " . Please check if you supplied the right one on the unity panel. Will shut down now.");
+            Application.Quit();
+        }
+    }
+
+    void Update()
+    {
+        try
+        {
+            if (String.Compare("K",interfaceStream.ReadLine()) <= 0)
+            {
+                butterflyAnimator.SetTrigger("Scared");
+                //butterflyAnimator.ResetTrigger("Scared");
+
+            }
+        }
+        catch (TimeoutException e) {
+            Debug.Log(e);
+        }
+    }
+
+    void OnDisable ()
+    {
+        interfaceStream.Close();
+    }
+
+    public void WriteToArduino (string m)
+    {
+        interfaceStream.WriteLine(m);
+        interfaceStream.BaseStream.Flush();
+    }
+
+}
---
+++ 4-24/Assets/ScaringTheNewButterfly/Butterfly/Credit.md
@@ -0,0 +1,4 @@
+# Source
+
+This package was edited by Jas Brooks for the University of Chicago's CMSC 23220: Inventing, Engineering and Understanding Interactive Devices course (HCIeng19). The original version can be found at the [Unity Asset Store](https://assetstore.unity.com/packages/3d/characters/animals/butterfly-animated-58355), and is by [INNOWELL GmbH](http://www.innowell.eu/).
+
